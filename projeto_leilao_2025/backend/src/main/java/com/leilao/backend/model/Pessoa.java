4f578956cf1bbf8b130578a5a997051fa5c197f2
educeciliato <148019576+educeciliato@users.noreply.github.com>
2025-07-28 21:34:53 -0300

    Update Pessoa.java

--- projeto_leilao_2025/backend/src/main/java/com/leilao/backend/model/Pessoa.java
+++ projeto_leilao_2025/backend/src/main/java/com/leilao/backend/model/Pessoa.java
@@ -1,21 +1,36 @@
 package com.leilao.backend.model;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.annotation.Generated;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 @Entity
 @Data
 @Table(name = "pessoa")
-public class Pessoa {
+public class Pessoa implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -24,6 +39,33 @@ public class Pessoa {
     @NotBlank(message = "{validation.email.notblank}")
     @Email(message = "{validation.email.notvalid}")
     private String email;
+    @JsonIgnore
     private String senha;
 
+    @OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @Setter(value = AccessLevel.NONE)
+    private List<PessoaPerfil> pessoaPerfil;
+
+    public void setPessoaPerfil(List<PessoaPerfil> pessoaPerfil) {
+        for (PessoaPerfil p : pessoaPerfil) {
+            p.setPessoa(this);
+        }
+        this.pessoaPerfil = pessoaPerfil;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return pessoaPerfil.stream().map(user -> new SimpleGrantedAuthority(user.getPerfil().getNome()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
